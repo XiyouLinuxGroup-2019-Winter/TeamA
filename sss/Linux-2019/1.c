7d8f8c2d3d7deb96e8b0699b94b5347b5ac8e40b
ssslql <2140327352@qq.com>
2020-02-15 15:25:33 +0800

    .

---
+++ sss/Linux-2019/1.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(int argc,char *argv[])
+{
+	for(unsigned int i=3;i>=0;i--)
+	putchar('=');
+}
+
---
+++ sss/Linux-2019/5.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+	int d='a'+1;
+	char ch=255;
+	printf("%d %d",ch,d);
+}
+
