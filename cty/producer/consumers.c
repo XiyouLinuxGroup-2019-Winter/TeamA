c720cae407a47567e1d5d650b60b2c3402919218
ensembleLocry <ensemblelocry@localhost.localdomain>
2020-06-01 23:25:20 +0800

    0

--- cty/producer/consumers.c
+++ cty/producer/consumers.c
@@ -8,10 +8,10 @@
 #include <unistd.h> 
 
 #define BUFFER_SIZE 5 
-typedef int buffer_item;
+
 
 sem_t empty, full, mutex;
-buffer_item buffer[BUFFER_SIZE];
+int buffer[BUFFER_SIZE];
 int in, out;
 struct data {
     int id;
@@ -19,9 +19,13 @@ struct data {
     int lastTime;
     int productId;
 };
-int insert_item(buffer_item item) { 
-/* insert item into buffer */ 
-buffer[out] = item; 
+int insert_item(buffer_item item) {  
+bsem_destroy(&mutex);
+    sem_destroy(&empty);
+    sem_destroy(&full);
+
+    return 0;
+}uffer[out] = item; 
 out = (out + 1) % BUFFER_SIZE; 
 return 0; 
 }
@@ -37,7 +41,12 @@ int opTime = ((struct data*)param)->opTime;
 int id = ((struct data*)param)->id;
 free(param);
 sleep(opTime);
-sem_wait(&empty);
+sem_destroy(&mutex);
+    sem_destroy(&empty);
+    sem_destroy(&full);
+
+    return 0;
+}sem_wait(&empty);
 sem_wait(&mutex);
 
 insert_item(productId);
@@ -49,4 +58,52 @@ printf("Thread %d: Producer produce %d\n", id, productId);
   pthread_exit(0);
 }
 
+void *consumer(void* param) { 
+int lastTime = (
+	(struct data*)param)->lastTime; 
+	int opTime = ((struct data*)param)->opTime; 
+	int id = ((struct data*)param)->id; 
+	free(param); 
+	sleep(opTime); 
+	sem_wait(&full); 
+	sem_wait(&mutex);  
+	buffer_item item; 
+	remove_item(&item); 
+	sleep(lastTime); 
+	printf("Thread %d: Consumer consume %d\n", id, item); 
+	sem_post(&mutex); 
+	sem_post(&empty); 
+	pthread_exit(0); 
+}
 
+int main() 
+{
+	pthread_t tid; 
+	pthread_attr_t attr; 
+	pthread_attr_init(&attr); 
+	sem_init(&mutex, 0, 1); 
+	sem_init(&empty, 0, BUFFER_SIZE); 
+	sem_init(&full, 0, 0); 
+	in = out = 0; 
+	int id = 0; 
+while(scanf("%d", &id) != EOF) 
+{ 
+	char role; 
+	id scanf("%c%d%d", &role, &opTime, &lastTime); 
+	struct data* d = (struct 	data*)malloc(sizeof(struct data)); 
+	d->id = id; d->opTime = opTime; 
+	d->lastTime = lastTime; 
+if(role == 'P') 
+{ 
+	scanf("%d", &productId); 
+	d->productId = productId; 
+	pthread_create(&tid, &attr, producer, d); 
+} 
+else if(role == 'C') 
+	pthread_create(&tid, &attr, consumer, d); }
+	sem_destroy(&mutex);
+    sem_destroy(&empty);
+    sem_destroy(&full);
+
+    return 0;
+}
