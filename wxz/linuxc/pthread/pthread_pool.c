cc1a207de058234be8defd38bd7fb935fae3d3eb
Taciturn-wu <3100563328@qq.com>
2020-06-08 23:52:25 +0800

    commit

--- wxz/linuxc/pthread/pthread_pool.c
+++ wxz/linuxc/pthread/pthread_pool.c
@@ -10,88 +10,325 @@
 #include <unistd.h>
 #include <pthread.h>
 #include <string.h>
-#define LEFT 30000000
-#define RIGHT 30000200
-#define NUM 4
-pthread_mutex_t mutex=PTHREAD_MUTEX_INITIALIZER;
-int num=0;
+#include <stdbool.h>
+#include <signal.h>
+#include <assert.h>
+#include <errno.h>
 
-void *pthread_pool(void* arg)
+
+typedef struct {
+    void *(*function)(void*);
+    void* arg;
+}threadpool_task_t;
+
+
+typedef struct threadpool_t
 {
-    int i,j,flag;
-    while(1)
+    pthread_mutex_t lock;
+    pthread_mutex_t thread_counter;
+
+    pthread_cond_t queue_not_full;
+    pthread_cond_t queue_not_empty;
+
+    pthread_t *threads;
+    pthread_t adjust_tid;
+    threadpool_task_t *task_queue;
+
+    int min_thr_num;
+    int max_thr_num;
+    int live_thr_num;
+    int busy_thr_num;
+    int wait_exit_thr_num;
+
+    int queue_front;                  
+    int queue_rear;  
+    int queue_size;
+    int queue_max_size;
+    
+    int shutdown;
+}threadpool_t;
+
+threadpool_t *thread_create(int min_thr_num,int max_thr_num,int queue_max_size);
+void *threadpool_thread(void *threadpool);
+void *adjust_thread(void *threadpool);
+int is_thread_alive(pthread_t tid);
+int threadpool_free(threadpool_t *pool);
+int threadpool_destroy(threadpool_t *pool);
+
+
+//threadpool_create(3,100,100);
+threadpool_t *thread_create(int min_thr_num,int max_thr_num,int queue_max_size)
+{
+    int i;
+    threadpool_t *pool=NULL;
+
+    do
     {
-        pthread_mutex_lock(&mutex);
-        while(num==0)
+        if((pool=(threadpool_t*)malloc(sizeof(threadpool_t)))==NULL)
         {
-            pthread_mutex_unlock(&mutex);
-            sched_yield();
-            pthread_mutex_unlock(&mutex);
-        }
-        if(num==-1)
-        {
-            pthread_mutex_unlock(&mutex);
+            printf("malloc fail");
             break;
         }
-        i=num;
-        num=0;
-        pthread_mutex_unlock(&mutex);
 
-        flag=1;
-        for(j=2;j<i/2;j++)
+        pool->min_thr_num=min_thr_num;
+        pool->max_thr_num=max_thr_num;
+        pool->busy_thr_num=0;
+        pool->live_thr_num=min_thr_num;
+        pool->wait_exit_thr_num=0;
+        pool->queue_size=0;
+        pool->queue_max_size=queue_max_size;
+        pool->queue_front=0;
+        pool->queue_rear=0;
+        pool->shutdown=false;
+
+        pool->threads=(pthread_t *)malloc(sizeof(pthread_t)*max_thr_num);
+        if(pool->threads==NULL)
         {
-            if(i%j==0)
+            printf("malloc fail");
+            break;
+        }
+
+        memset(pool->threads,0,sizeof(pthread_t)*max_thr_num);
+
+        pool->task_queue=(threadpool_task_t*)malloc(sizeof(threadpool_task_t)*queue_max_size);
+        if(pool->task_queue==NULL)
+        {
+            printf("malloc fail");
+            break;
+        }
+
+        if( pthread_mutex_init(&(pool->lock),NULL)!=0 ||
+            pthread_mutex_init(&(pool->thread_counter),NULL)!=0 ||
+            pthread_cond_init(&(pool->queue_not_full),NULL)!=0 ||
+            pthread_cond_init(&(pool->queue_not_empty),NULL)!=0)
             {
-                flag=0;
+                printf("init the lock or cond fail");
                 break;
             }
-        }
-        if(flag)
+
+        for(i=0;i<min_thr_num;i++)
         {
-            printf("[%d]%d is a pthread\n",(int)arg,i);
+            pthread_create(&(pool->threads[i]),NULL,threadpool_thread,(void*)pool);
+            printf("start thread %x ------\n",(unsigned int)pool->threads[i]);
         }
-        pthread_exit(NULL);
+        pthread_create(&(pool->adjust_tid),NULL,adjust_thread,(void*)pool);
+        return pool;
+    } while (0);
+
+    threadpool_free(pool);
+    return NULL;
+}
+
+int threadpool_add(threadpool_t *pool,void *(*function)(void* arg),void *arg)
+{
+    pthread_mutex_lock(&(pool->lock));
+
+    while((pool->queue_size==pool->queue_max_size)&&(!pool->shutdown))
+    {
+        pthread_cond_wait(&(pool->queue_not_full),&(pool->lock));
+
     }
+
+    if(pool->shutdown)
+    {
+        pthread_mutex_unlock(&(pool->lock));
+    }
+
+    if(pool->task_queue[pool->queue_rear].arg!=NULL)
+    {
+        pool->task_queue[pool->queue_rear].arg=NULL;
+    }
+
+    pool->task_queue[pool->queue_rear].function=function;
+    pool->task_queue[pool->queue_rear].arg=arg;
+
+    pool->queue_rear=(pool->queue_rear+1)%pool->queue_max_size;
+    pool->queue_size++;//向任务队列中添加一个任务
+
+    pthread_cond_signal(&(pool->queue_not_empty));
+    pthread_mutex_unlock(&(pool->lock));
+}
+int threadpool_destroy(threadpool_t *pool)
+{
+    int i;
+    if(pool==NULL)
+        return -1;
+
+    pool->shutdown=true;
+
+    pthread_join(pool->adjust_tid,NULL);
+
+    for(i=0;i<pool->live_thr_num;i++)
+    {
+        pthread_cond_broadcast(&(pool->queue_not_empty));
+    }
+
+    for(i=0;i<pool->live_thr_num;i++)
+    {
+        pthread_join(pool->threads[i],NULL);
+    }
+    threadpool_free(pool);
+}
+int threadpool_free(threadpool_t *pool)
+{
+    if (pool==NULL) 
+        return -1;
+
+    if (pool->task_queue) 
+    {
+        free(pool->task_queue);
+    }
+    if (pool->threads) 
+    {
+        free(pool->threads);
+        pthread_mutex_lock(&(pool->lock));
+        pthread_mutex_destroy(&(pool->lock));
+        pthread_mutex_lock(&(pool->thread_counter));
+        pthread_mutex_destroy(&(pool->thread_counter));
+        pthread_cond_destroy(&(pool->queue_not_empty));
+        pthread_cond_destroy(&(pool->queue_not_full));
+    }
+    free(pool);
+    pool=NULL;
+
+}
+
+int is_thread_alive(pthread_t tid)
+{
+    int kill_rc=pthread_kill(tid, 0);     
+    if(kill_rc==ESRCH) 
+    {
+        return false;
+    }
+
+    return true;
+}
+
+
+void *threadpool_thread(void* threadpool)
+{
+    threadpool_t *pool=(threadpool_t *)threadpool;
+    threadpool_task_t task;
+    while(1)
+    {
+        pthread_mutex_lock(&(pool->lock));
+
+        while((pool->queue_size==0) && (!pool->shutdown))
+        {
+            pthread_cond_wait(&(pool->queue_not_empty),&(pool->lock));
+
+            if(pool->wait_exit_thr_num>0)
+                pool->wait_exit_thr_num--;
+            
+            if(pool->live_thr_num>pool->min_thr_num)
+            {
+                pool->live_thr_num--;
+                pthread_mutex_unlock(&(pool->lock));
+                pthread_exit(NULL);
+            }
+        }
+    }
+
+    if(pool->shutdown)
+
+
+    task.function=pool->task_queue[pool->queue_front].function;
+    task.arg=pool->task_queue[pool->queue_front].arg;
+
+    pool->queue_front=(pool->queue_front+1)%pool->queue_max_size;
+    pool->queue_size--;
+
+    pthread_cond_signal(&(pool->queue_not_full));
+
+    pthread_mutex_unlock(&(pool->lock));
+
+    pthread_mutex_lock(&(pool->lock));
+    pool->busy_thr_num++;
+    pthread_mutex_unlock(&(pool->lock));
+
+    (*(task.function))(task.arg);    
+    //task.function(task.arg);
+
+    pthread_mutex_lock(&(pool->lock));
+    pool->busy_thr_num--;
+    pthread_mutex_unlock(&(pool->lock));
+    
+    
+}
+void *adjust_thread(void* threadpool)
+{
+    int i;
+    threadpool_t* pool=(threadpool_t*)threadpool;
+    while(!pool->shutdown)
+    {
+        sleep(10);
+        pthread_mutex_lock(&(pool->lock));
+        int queue_size=pool->queue_size;
+        int live_thr_num=pool->live_thr_num;
+        pthread_mutex_unlock(&(pool->lock));
+
+        pthread_mutex_lock(&(pool->thread_counter));
+        int busy_thr_num=pool->busy_thr_num;
+        pthread_mutex_unlock(&(pool->thread_counter));
+
+        if(queue_size>=10 && live_thr_num < pool->max_thr_num) 
+        {
+            pthread_mutex_lock(&(pool->lock));  
+            int add = 0;
+         
+            for(i=0;i<pool->max_thr_num && add<10
+                    && pool->live_thr_num < pool->max_thr_num; i++) 
+            {
+                if (pool->threads[i] == 0 || !is_thread_alive(pool->threads[i])) 
+                {
+                    pthread_create(&(pool->threads[i]),NULL,threadpool_thread,(void *)pool);
+                    add++;
+                    pool->live_thr_num++;
+                }
+            }
+            pthread_mutex_unlock(&(pool->lock));
+        }
+
+        
+        if ((busy_thr_num * 2) < live_thr_num  &&  live_thr_num > pool->min_thr_num)
+        {
+
+            pthread_mutex_lock(&(pool->lock));
+            pool->wait_exit_thr_num = 10;      /* 要销毁的线程数 设置为10 */
+            pthread_mutex_unlock(&(pool->lock));
+
+            for(i=0;i<10; i++) 
+            {
+                pthread_cond_signal(&(pool->queue_not_empty));
+            }
+        }
+    }
+
+    return NULL;
+}
+void *process(void* arg)
+{
+    printf("thread 0x%x working on task %d\n ",(unsigned int)pthread_self(),*(int *)arg);
+    sleep(1);
+    printf("task %d is end\n",*(int *)arg);
+
+    return NULL;
 }
 int main()
 {
-    int i,ret;
-    pthread_t tid[NUM];
-    for(i=0;i<NUM;i++)
+    //threadpool_t *thread_create(int min_thr_num,int max_thr_num,int queue_max_size)
+    threadpool_t *thp=thread_create(3,100,100);
+
+    int num[20],i;
+    for(i=0;i<20;i++)
     {
-        ret=pthread_create(tid+i,NULL,pthread_pool,(void*)i);
-        if(ret)
-        {
-            fprintf(stderr,"pthread_create():%s\n",strerror(ret));
-            exit(1);
-        }
+        num[i]=i;
+        printf("add task %d\n",num[i]);
+
+        //int threadpool_add(threadpool_t *pool,void *(*function)(void* arg),void *arg)
+        threadpool_add(thp,process,(void*)&num[i]);
     }
 
-    for(i=LEFT;i<=RIGHT;i++)
-    {
-        pthread_mutex_lock(&mutex);
-        while(num!=0)
-        {
-            pthread_mutex_unlock(&mutex);
-            //sleep(1);
-            sched_yield();
-            pthread_mutex_lock(&mutex);
-        }
-        num=i;
-        pthread_mutex_unlock(&mutex);
-    }
-
-    pthread_mutex_lock(&mutex);
-    while(num!=0)
-    {
-        pthread_mutex_unlock(&mutex);
-        sched_yield();
-        pthread_mutex_lock(&mutex);
-    }
-    num=-1;
-    pthread_mutex_unlock(&mutex);
-
-    for(i=0;i<NUM;i++)
-        pthread_join(tid[i],NULL);
-    pthread_mutex_destroy(&mutex);
+    sleep(10);
+    threadpool_destroy(thp);
 }
--- wxz/linuxc/socket/epoll_heap.c
+++ wxz/linuxc/socket/epoll_heap.c
@@ -3,6 +3,7 @@
 
 void recvdata(int fd,int events,void* arg);
 void senddata(int fd,int events,void* arg);
+void acceptconnet(int lfd,int events,void* arg);
 struct myevent_s{
     int fd;
     int events;
@@ -45,35 +46,18 @@ void eventadd(int epfd,int events,struct myevent_s* ev)
     epoll_ctl(epfd,op,ev->fd,&epv);
 
 }
-int main(int argc,char *argv[])
+
+void eventdel(int epfd,struct myevent_s* ev)
 {
-    unsigned short port=SERV_PORT;
-    if(argc==2)
-        port=atoi(argv[1]);
+    struct epoll_event epv;
+    if(ev->status!=1)
+        return ;
     
-    epfd=epoll_create(1025);
-    initsocket(epfd,port);
-
-    struct epoll_event events[1025];
-    int i;
-    
-    int n=epoll_wait(epfd,events,1025,1000);
-
-    for(i=0;i<n;i++)
-    {
-        struct myevent_s *ev=(struct myevent_s*)events[i].data.ptr;
-        if((events[i].events &EPOLLIN)&&(ev->events &EPOLLIN))
-        {
-            //void (*call_back)(int fd,int events,void* arg);
-            ev->call_back(ev->fd,events[i].events,ev->arg);
-        }
-        if((events[i].events &EPOLLOUT)&&(ev->events &EPOLLOUT))
-        {
-            //void (*call_back)(int fd,int events,void* arg);
-            ev->call_back(ev->fd,events[i].events,ev->arg);
-        }
-    }
+    epv.data.ptr=NULL;
+    ev->status=0;
+    epoll_ctl(epfd,EPOLL_CTL_DEL,ev->fd,&epv);
 }
+
 void initsocket(int epfd,short port)
 {
     int lfd;
@@ -82,7 +66,7 @@ void initsocket(int epfd,short port)
 
     struct sockaddr_in serv_addr;
 
-    memset(&serv_addr,0,siezof(serv_addr));
+    memset(&serv_addr,0,sizeof(serv_addr));
     serv_addr.sin_family=AF_INET;
     serv_addr.sin_port=htons(SERV_PORT);
     serv_addr.sin_addr.s_addr=htonl(INADDR_ANY);
@@ -118,4 +102,103 @@ void acceptconnet(int lfd,int events,void* arg)
         eventset(&g_events[i],cfd,recvdata,&g_events[i]);
     } while (0);
     
+}
+void recvdata(int fd,int events,void* arg)
+{
+    struct myevent_s* ev=(struct myevent_s*)arg;
+
+    int len=recv(fd,ev->buf,sizeof(ev->buf),0);
+
+    eventdel(epfd,ev);
+
+    if(len>0)
+    {
+        ev->len=len;
+        ev->buf[len]='\0';
+
+        printf("C[%d]:%s\n", fd, ev->buf);
+
+        eventset(ev,fd,senddata,ev);
+        eventadd(epfd,EPOLLOUT,ev);
+    }
+    else if(len==0)
+    {
+        close(ev->fd);
+    }
+    else
+    {
+        close(ev->fd);
+    }
+    
+}
+void senddata(int fd,int events,void* arg)
+{
+    struct myevent_s *ev=(struct myevent_s*)arg;
+
+    int len=send(fd,ev->buf,ev->len,0);
+
+    eventdel(epfd,ev);
+
+    if(len>0)
+    {
+        printf("send[fd=%d], [%d]%s\n", fd, len, ev->buf);
+        eventset(ev,fd,recvdata,ev);
+        eventadd(epfd,EPOLLIN,ev);
+    }
+    else
+    {
+        close(ev->fd);
+    }
+    
+}
+int main(int argc,char *argv[])
+{
+    unsigned short port=SERV_PORT;
+    if(argc==2)
+        port=atoi(argv[1]);
+    
+    epfd=epoll_create(1025);
+    initsocket(epfd,port);
+
+    struct epoll_event events[1025];
+    int i;
+    int checkpos=0;
+
+    while(1)
+    {
+        long now=time(NULL);
+        for(i=0;i<100;i++,checkpos++)
+        {
+            if(checkpos==1024)
+                checkpos=0;
+            if(g_events[checkpos].status!=1)
+                continue;
+
+            long duration=now-g_events[checkpos].last_active;
+
+            if(duration>=60)
+            {
+                close(g_events[checkpos].fd);
+                eventdel(epfd,&g_events[checkpos]);
+            }
+        }
+    
+    
+        int n=epoll_wait(epfd,events,1025,1000);
+
+        for(i=0;i<n;i++)
+        {
+            struct myevent_s *ev=(struct myevent_s*)events[i].data.ptr;
+            if((events[i].events &EPOLLIN)&&(ev->events &EPOLLIN))
+            {
+                //void (*call_back)(int fd,int events,void* arg);
+                ev->call_back(ev->fd,events[i].events,ev->arg);
+            }
+            if((events[i].events &EPOLLOUT)&&(ev->events &EPOLLOUT))
+            {
+                //void (*call_back)(int fd,int events,void* arg);
+                ev->call_back(ev->fd,events[i].events,ev->arg);
+            }
+        }
+    }
 }
