14cfe6be1f6934ea52fb860f2ff4d964223b9af1
Taciturn-wu <3100563328@qq.com>
2020-07-05 05:30:30 +0800

    commit

--- wxz/ordinary/其他/buy.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main()
-{
-	int a,b,n;
-	scanf("%d%d",&a,&b);
-	n=(10*a+b)/19;
-	printf("%d\n",n);
-}
--- wxz/ordinary/其他/test1.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-int main(int argc,char* argv[])
-{
-	char* p="Hello World";
-	char q[]="Hello World";
-}
