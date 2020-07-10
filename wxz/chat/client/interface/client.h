4433785dc8d3f44b1b7120768dc1d6fee372f885
Taciturn-wu <3100563328@qq.com>
2020-07-10 17:49:47 +0800

    commit

--- wxz/chat/client/interface/client.h
+++ wxz/chat/client/interface/client.h
@@ -3,6 +3,54 @@
 #include <string.h>
 
 #define MAX 50
+#define MAX_CHAR         300
+
+typedef struct  friend_info
+{
+    int statue;
+    //好友的信息数
+    int message_num;
+    char name[MAX_CHAR];
+}FRIEND_INFO;
+
+typedef struct  user_group_info
+{
+    char group_name[20];
+    int types[10];//群中职位
+    int num;
+    int group_member_num;
+    char group_memeber_name[20][20];
+    int statue[10];
+}USER_GROUP_INFO;
+
+typedef struct user_infor
+{
+    char username [MAX_CHAR];
+    FRIEND_INFO friends [MAX_CHAR];
+    int friends_num;
+    int group_num;
+    char group [MAX_CHAR][MAX_CHAR];
+}USER_INFO;
+
+typedef struct data
+{
+    char send_name[20];
+    char recv_name[20];
+    int send_fd;
+    int recv_fd;
+
+   char message[MAX_CHAR*2];
+}DATA;
+
+typedef struct package
+{
+    int type;
+    DATA data;
+}PACK;
+
+
+
+
 typedef struct person_account
 {
     int flag;
---
+++ wxz/chat/client/interface/friend.c
@@ -0,0 +1,9 @@
+#include <client.h>
+void add_friend()
+{
+    char name_t[MAX_CHAR];
+    printf("请输入要添加好友的账号:");
+    scanf("%s",name_t);
+    Clear_buffer();
+    
+}
