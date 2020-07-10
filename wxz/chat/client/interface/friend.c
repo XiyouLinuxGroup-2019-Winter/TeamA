f9d130020703652ca3ce907cc5441538131195fb
Taciturn-wu <3100563328@qq.com>
2020-07-10 22:06:22 +0800

    commit

--- wxz/chat/client/interface/friend.c
+++ wxz/chat/client/interface/friend.c
@@ -5,5 +5,13 @@ void add_friend()
     printf("请输入要添加好友的账号:");
     scanf("%s",name_t);
     Clear_buffer();
+    char str[MAX_CHAR];
+    sprintf(str,"%s",name_t);
+    if(Send_message(6,name_t,"")==1);
+        printf("好友请求发送成功");
+    return;
+}
+void del_friend()
+{
     
 }
